d4b53bdc9c93f5e63a73f7d316778393225e659b
irakli kvachahia <iraklion93@mail.ru>
2023-04-05 14:48:52 +0400

    RadixSort optimization

--- sorting/src/main/java/radix_sort/RadixSort.java
+++ sorting/src/main/java/radix_sort/RadixSort.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
  * по результатам сравнения значений этого разряда внутри образованных на предыдущем проходе групп,
  * либо переупорядочиваются в целом, но сохраняя относительный порядок, достигнутый при предыдущей сортировке.
  * Затем аналогично делается для следующего разряда, и так до конца.
- *<br> <a href="https://www.cs.usfca.edu/~galles/visualization/RadixSort.html">визуализация</a>
+ * <br> <a href="https://www.cs.usfca.edu/~galles/visualization/RadixSort.html">визуализация</a>
  */
 public class RadixSort implements Sort {
 
@@ -25,33 +25,24 @@ public class RadixSort implements Sort {
     @Override
     public int[] sort() {
         //массив для подсчета колличества совпадений(индекс сопоставляется со значением оперируемого разряда)
-        int [] numbers = new int[10];
+        int[] numbers = new int[10];
         //макисмальная длина числа
         int length = calculateMaximumLengthNumber();
-        //для рассчета необходимого разряда числа на шаге
-        int index = 0;
-
+        //модуль для получения разряда
+        int range = 10;
+        //коэфициент для поиска индекса
+        int placeValue = 10;
         while (length > 0) {
-
             int number = 0;
             //массив для записи значений оперируемого разряда
-            int [] numberArray = new int[array.length];
-
+            int[] numberArray = new int[array.length];
             //проходимся по массиву, достаем значение из разряда по порядку (index - указатель на разряд)
             for (int i = 0; i < array.length; i++) {
-                //получаем поразрядно число
-                String[] numberStr = String.valueOf(array[i]).split("");
-                if(numberStr.length > index) {
-                    //если текущий разряд не превышает разряд числа, то записываем его в массив numberArray
-                    //и увеличиваем счетчик для number (число совпадает с индексом)
-                    number = Integer.valueOf(numberStr[numberStr.length - index - 1]);
-                    numbers[number] = numbers[number] + 1;
-                    numberArray[i] = number;
-                } else {
-                    //если текущий разряд превысил разряд числа, то считаем его равным 0
-                    numbers[0] = numbers[0] + 1;
-                    numberArray[i] = 0;
-                }
+                //если текущий разряд не превышает разряд числа, то записываем его в массив numberArray
+                //и увеличиваем счетчик для number (число совпадает с индексом)
+                number = (array[i] / placeValue) % range; //получаем поразрядно число
+                numbers[number] = numbers[number] + 1;
+                numberArray[i] = number;
             }
 
             //суммируем соседние счетчики для получения информации о том,
@@ -61,7 +52,7 @@ public class RadixSort implements Sort {
             }
 
             //массив для записи результата
-            int [] newArr = new int[array.length];
+            int[] newArr = new int[array.length];
 
             //проходимся по значением цифр из текущего разряда
             //и в соответвии со счетчиком numbers выставляем значения в новый массив
@@ -76,23 +67,24 @@ public class RadixSort implements Sort {
             array = newArr;
             //возвращаем счетчик в первоначальное состояние
             Arrays.fill(numbers, 0);
-            //увеличивем разрядность
-            index++;
             //уменьшаем длину
             length--;
+            //увеличивем коэфициент для поиска индекса
+            placeValue *= 10;
         }
         return array;
     }
 
     /**
      * вычисляем максимальную длину числа
+     *
      * @return максимальная длина числа
      */
     private int calculateMaximumLengthNumber() {
         int length = 0;
         for (int i = 0; i < array.length; i++) {
             int elementLength = String.valueOf(array[i]).length();
-            if(length <= elementLength) {
+            if (length <= elementLength) {
                 length = elementLength;
             }
         }
@@ -101,6 +93,5 @@ public class RadixSort implements Sort {
 
     @Override
     public void swap(int x, int y) {
-
     }
 }
